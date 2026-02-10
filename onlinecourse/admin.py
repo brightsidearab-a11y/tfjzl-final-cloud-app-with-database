301a2cc6b84d5f1b0032ea65169b5f1576bd41a4
brightsidearab-a11y <brightsidearab@gmail.com>
2026-02-10 11:38:18 +0100

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,1 +1,31 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
