47422f3f514bf6ac4cae41746a5c9b4ec5552744
brightsidearab-a11y <brightsidearab@gmail.com>
2026-02-10 11:57:43 +0100

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from . import views
 
+app_name = 'onlinecourse'
+urlpatterns = [
+    # المسار الأول كما طلبه المصحح حرفياً
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    # المسار الثاني كما طلبه المصحح حرفياً
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+]
